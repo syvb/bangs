110d4666b0a2dfc89b755d11c0dca6af84b6a3fa
Smitty <me@smitop.com>
2021-02-04 17:52:37 -0500

    make case insensitive

--- src/lib.rs
+++ src/lib.rs
@@ -13,17 +13,18 @@ impl Bang {
     pub fn parse_search(s: &str) -> Option<(Self, String)> {
         lazy_static! {
             // \s requires Unicode support
-            static ref PARSE_REGEX: Regex = Regex::new(r#"(!([A-Za-z0-9]+([ \t\r\n\f\u00A0]|$)))"#).unwrap();
+            static ref PARSE_REGEX: Regex = Regex::new(r#"(!([a-z0-9]+([ \t\r\n\f\u00A0]|$)))"#).unwrap();
         }
-        let range = PARSE_REGEX.find(s)?.range();
+        let s = s.to_ascii_lowercase();
+        let range = PARSE_REGEX.find(&s)?.range();
         if !s.is_char_boundary(range.start) || !s.is_char_boundary(range.start + 1) || !s.is_char_boundary(range.end) {
             return None;
         }
-        let mut search = String::from(s);
-        search.replace_range(range.clone(), "");
+        let mut search_without_bang = s.clone();
+        search_without_bang.replace_range(range.clone(), "");
         Some((
             bangs::lookup_bang(s[(range.start + 1)..range.end].trim())?,
-            search,
+            search_without_bang,
         ))
     }
     pub fn with_query(&self, q: &str) -> String {
